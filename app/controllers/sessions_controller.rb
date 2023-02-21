c82dc7fc86a09240a4c9c732b26c5b856dd825c9
vindhyatummala <vindhya.tummala@kellogg.northwestern.edu>
2023-02-21 20:21:42 +0000

    Coding along

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,22 @@
+class SessionsController < ApplicationController
+    
+    def new
+    end
+
+    def create
+        #check if the user exists
+        @user = User.find_by({"email" => params["email"]})
+        if @user
+        #if they do, check that they know their password
+            if params["password"] == @user["password"]
+                #if they do, send them to..
+                redirect_to "/companies"
+            else 
+                redirect_to "/sessions/new"
+            end
+        else
+            redirect_to "/sessions/new"
+        end
+    end
+
+end
